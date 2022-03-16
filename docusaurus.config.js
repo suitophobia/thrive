55e78cc1d42ee3e4e24a4eb9e1254ead19fb0989
Stephen van der Heijden <stephengrantvdh@gmail.com>
2022-03-16 17:14:16 +0100

    Updates config files

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,13 +8,13 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 const config = {
   title: 'My Site',
   tagline: 'Dinosaurs are cool',
-  url: 'https://your-docusaurus-test-site.com',
-  baseUrl: '/',
+  url: 'https://suitophobia.github.io/',
+  baseUrl: '/thrive/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'suitophobia', // Usually your GitHub org/user name.
+  projectName: 'thrive', // Usually your repo name.
 
   presets: [
     [
